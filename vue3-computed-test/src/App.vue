c2b39de10ebb1b3c074f61ae51436e88339b8ffe
JWooni <wjddns59@naver.com>
2021-05-22 16:57:34 +0900

    commit

---
+++ vue3-computed-test/src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <h1>{{ reversedMessage }}</h1>
+  <h1>{{ reversedMessage }}</h1>
+  <h1>{{ reversedMessage }}</h1>
+  <h1>{{ reversedMessage }}</h1>
+</template>
+
+<script>
+export default {
+  data(){
+    return{
+      msg: 'Hello Computed!'
+    }
+  },
+  computed:{
+    reversedMessage(){
+      return this.msg.split('').reverse().join('')
+
+    }
+  },
+  methods: {
+    reverseMessage(){
+      return this.msg.split('').reverse().join('')
+    }
+  }
+}
+</script>
---
+++ vue3-webpack-template/src/components/HelloWorld.vue
@@ -0,0 +1,6 @@
+<template>
+  <img
+    src="~assets/chelsea.jpg"
+    alt="첼시" />
+  <div></div>
+</template>
