8c6635462533c0279c1ed9792fd7124ce86e41dd
JWooni <wjddns59@naver.com>
2021-05-23 01:40:48 +0900

    commit

--- vue3-computed-test/src/App.vue
+++ vue3-computed-test/src/App.vue
@@ -1,27 +1,72 @@
 <template>
+  <!-- <button @click="add">
+    ADD
+  </button> -->
+  <!-- <h1 @click="changeMessage">
+    {{ msg }}
+  </h1> -->
+  <!-- <h1>{{ reversedMessage }}</h1> -->
+  <!-- <h1>{{ reversedMessage }}</h1>
   <h1>{{ reversedMessage }}</h1>
-  <h1>{{ reversedMessage }}</h1>
-  <h1>{{ reversedMessage }}</h1>
-  <h1>{{ reversedMessage }}</h1>
+  <h1>{{ reversedMessage }}</h1> -->
+  <h1
+    :class="{active:isActive}"
+    @click="activate">
+    Hello?!({{ isActive }})
+  </h1>
 </template>
 
 <script>
 export default {
   data(){
     return{
-      msg: 'Hello Computed!'
-    }
+      // Getter, Setter
+      //msg: 'Hello Computed!'
+      // msg: 'Hello?'
+      isActive: false
+}
   },
-  computed:{
-    reversedMessage(){
-      return this.msg.split('').reverse().join('')
-
-    }
-  },
+  //computed:{
+    // Getter
+    // reversedMessage(){
+    //   return this.msg.split('').reverse().join('')
+    //  }
+    // Getter, Setter
+    // reversedMessage: {
+    //   get(){
+    //     return this.msg.split('').reverse().join('')
+    //   },
+    //   set(value){
+    //     this.msg = value
+    //   }
+    // }
+  //},
+  // watch:{
+  //   msg(newValue){
+  //     console.log('msg:', newValue)
+  //   },
+  //   reversedMessage(newValue){
+  //     console.log('reversedMessage', newValue)
+  //   }
+  // },
   methods: {
-    reverseMessage(){
-      return this.msg.split('').reverse().join('')
+    // add(){
+    //   this.reversedMessage += '!?'
+    //   //this.msg += '?!'
+    // }
+    // changeMessage(){
+    //   this.msg = 'Good!'
+    // }
+    activate(){
+      this.isActive = true
     }
   }
 }
 </script>
+
+<style scoped>
+  .active{
+    color: red;
+    font-weight: bold;
+  }
+</style>
