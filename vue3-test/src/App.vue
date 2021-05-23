8c03ad82b17e2d31e0b85b2423fe9f1887177d04
JWooni <wjddns59@naver.com>
2021-05-24 02:03:51 +0900

    commit

--- vue3-test/src/App.vue
+++ vue3-test/src/App.vue
@@ -1,16 +1,38 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <img
+    alt="Vue logo"
+    src="./assets/logo.png" />
+  <HelloWorld msg="Welcome to Your Vue.js App" />
+
+  <!-- v-if -->
+  <button @click="handler">
+    click me!
+  </button>
+  <h1 v-if="isShow">
+    Hello?!
+  </h1>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// import HelloWorld from './components/HelloWorld.vue'
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  //name: 'App',
+  // components: {
+  //   HelloWorld
+  // }
+
+  data(){
+    return{
+      isShow: true
+    }
+  },
+  methods:{
+    handler(){
+      this.isShow = !this.isShow
+    }
   }
+
 }
 </script>
 
