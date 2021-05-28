71e9b8aca05f9cf400c18cbe0c468f66ba22b7f1
JWooni <wjddns59@naver.com>
2021-05-29 01:37:41 +0900

    20210529 commit

---
+++ vue3-test-vmodel/src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <h1>{{ msg }}</h1>
+  <!-- 한글 입력 장점 -->
+  <!-- <input
+    type="text"
+    :value="msg"
+    @change="msg = $event.target.value" /> -->
+
+  <input
+    type="text"
+    v-model.trim="msg" />
+</template>
+
+<script>
+export default {
+  data(){
+    return{
+      msg:'Hello world'
+    }
+  },
+  watch:{
+    msg(){
+      console.log(this.msg)
+    }
+  }
+}
+</script>
