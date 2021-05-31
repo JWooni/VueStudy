b624e937cd11cc98765aa5c7fb8a8e089e9fd766
JWooni <wjddns59@naver.com>
2021-06-01 01:34:08 +0900

    20210601 commit

---
+++ vue3-test-attr-inherit/src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <MyBtn
+    class="jwooni"
+    style="color:red"
+    title="Hello!">
+    Banana
+  </MyBtn>
+</template>
+
+<script>
+import MyBtn from '~/components/MyBtn'
+export default {
+  components: {
+    MyBtn
+  }
+}
+</script>
---
+++ vue3-test-attr-inherit/src/components/MyBtn.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="btn">
+    <slot></slot>
+  </div>
+  <h1 v-bind="$attrs"></h1>
+</template>
+
+<script>
+export default {
+  inheritAttrs: false,
+  created(){
+    console.log(this.$attrs)
+  }
+}
+</script>
+
+<style scoped>
+  .btn {
+    display: inline-block;
+    margin: 4px;
+    padding: 6px 12px;
+    border-radius: 4px;
+    background-color: gray;
+    color: white;
+    cursor: pointer;
+  }
+</style>
