e4a2057ee9cdeb92ba78afb8eadad9a91b85a8fc
JWooni <wjddns59@naver.com>
2021-06-02 00:48:45 +0900

    20210602

---
+++ vue3-test-emit/src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <MyBtn
+    @jwooni="log"
+    @change-msg="logMsg">
+    Banana
+  </MyBtn>
+</template>
+
+<script>
+import MyBtn from '~/components/MyBtn'
+export default {
+  components: {
+    MyBtn
+  },
+  methods:{
+    log(event){
+      console.log('Click!!')
+      console.log(event)
+    },
+    logMsg(msg) {
+      console.log(msg)
+    }
+  }
+}
+</script>
