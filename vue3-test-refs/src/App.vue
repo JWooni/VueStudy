35098d836284f803d716119aaf81e123dc658e19
JWooni <wjddns59@naver.com>
2021-06-03 18:54:09 +0900

    commit

---
+++ vue3-test-refs/src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <Hello ref="hello" />
+</template>
+
+<script>
+import Hello from '~/components/Hello'
+
+export default {
+  components: {
+    Hello
+  },
+  mounted() {
+    console.log(this.$refs.hello.$refs.good)
+  }
+}
+</script>
