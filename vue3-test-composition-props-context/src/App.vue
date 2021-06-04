529d53afe86f133c9cc82ec48c15b442126f9f1b
JWooni <wjddns59@naver.com>
2021-06-05 01:10:54 +0900

    commit

---
+++ vue3-test-composition-props-context/src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <MyBtn
+    class="jwooni"
+    style="color: red;"
+    @hello="log">
+    Apple
+  </MyBtn>
+</template>
+
+<script>
+import MyBtn from '~/components/MyBtn'
+
+export default {
+  components: {
+    MyBtn
+  },
+  methods: {
+    log() {
+      console.log('Hello world!')
+    }
+  }
+}
+</script>
